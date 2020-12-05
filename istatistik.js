82d375e2b76fde87c19efa390bc12c733b234aed
mayhem3 <71923340+mayhem3@users.noreply.github.com>
2020-12-05 19:31:14 +0300

    Update istatistik.js

--- istatistik.js
+++ istatistik.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 const os = require("os");
 require("moment-duration-format");
 exports.run = async (client, message, args) => {
-  const seksizaman = moment
+  const unutulmuşzaman = moment
     .duration(client.uptime)
     .format(" D [gün], H [saat], m [dakika], s [saniye]");
   const istatistikler = new Discord.MessageEmbed()
@@ -12,7 +12,7 @@ exports.run = async (client, message, args) => {
     .setFooter("© Warden Giveaway 2020", client.user.avatarURL())
     .addField("<:Ownership:780126411390189578> **|** **Botun Yapımcısı**", "<@743387541100822598>") .addField("<a:typing:778997305297731585> **|** **Ping**",client.ws.ping + " ms", false)
     .addField("<a:cloudup:778560542033182740> **|** **Bellek kullanımı**",(process.memoryUsage().heapUsed / 1024 / 512).toFixed(2) + " MB",true)
-    .addField("<a:crank:778975399464009739> **|** **Çalışma süresi**", seksizaman, true).addField("<:utandm:777095995690450944> **|** **Kullanıcılar**",client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
+    .addField("<a:crank:778975399464009739> **|** **Çalışma süresi**", unutulmuşzaman, true).addField("<:utandm:777095995690450944> **|** **Kullanıcılar**",client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
     .addField("<a:hypesquad_events:772428250915667968> **|** **Sunucular**",client.guilds.cache.size.toLocaleString(), true)
     .addField("💻 **|** **Kanallar**", client.channels.cache.size.toLocaleString(), true)
    .addField("<:js:778975699024871495> **|** **Discord.JS sürüm**", "v" + Discord.version, true)
